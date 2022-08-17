d704902c54a43c5d401a4b1dfd767e6b852c2113
BuildTools <unconfigured@null.spigotmc.org>
2022-08-17 10:31:32 -0500

    Initial commit

---
+++ src/main/java/me/woodsmc/livessmpplugin/LivesSMPPlugin.java
@@ -0,0 +1,17 @@
+package me.woodsmc.livessmpplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LivesSMPPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
